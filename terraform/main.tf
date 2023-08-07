978bec648e0e611c8f291f7af001e86e46dd1d7b
junah201 <junah.dev@gmail.com>
2023-08-08 03:11:09 +0900

    feat: Add terraform lambda & restapi

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,3 +46,46 @@ module "util_layer" {
   store_on_s3 = true
   s3_bucket   = aws_s3_bucket.lambda_build_bucket.id
 }
+
+// Lambda
+module "v1_generate_badge_lambda" {
+  depends_on = [aws_s3_bucket.lambda_build_bucket]
+
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = "v1__generate-badge"
+  description   = "Generate badge for SolvedAC"
+  handler       = "main.lambda_handler"
+  runtime       = "python3.10"
+  timeout       = 120
+  source_path   = "../lambdas/v1/generate_badge"
+
+  store_on_s3 = true
+  s3_bucket   = var.lambda_build_bucket
+
+  create_role = false
+  lambda_role = module.lambda_default_role.role_arn
+
+  layers = [
+    module.util_layer.lambda_layer_arn
+  ]
+
+  tags = {
+    version = "v1"
+  }
+}
+
+// rest api
+module "aws_api_gateway_route" {
+  source = "./modules/restapi"
+
+  name        = "solvedac-rest-api"
+  description = "solvedac readme rest api"
+
+  domain_certificate_name = "*.junah.dev"
+  domain_name             = "solvedac.junah.dev"
+
+  lambdas = [
+    module.v1_generate_badge_lambda.lambda_function_name
+  ]
+}
